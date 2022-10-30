341ef4bf03209601e382d4b6c2c1f331ec121bf2
miruser3 <97870721+miruser3@users.noreply.github.com>
2022-10-30 17:46:33 +0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const products = {
+    '1':{
+        "name":"ELLERY X M'O CAPSULE 1",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+    },
+    '2':{
+        "name":"ELLERY X M'O CAPSULE 2",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+    },
+    '3':{
+        "name":"ELLERY X M'O CAPSULE 3",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+    },
+    '4':{
+        "name":"ELLERY X M'O CAPSULE 4",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+    },
+    '5':{
+        "name":"ELLERY X M'O CAPSULE 5",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+    },
+    '6':{
+        "name":"ELLERY X M'O CAPSULE 6",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+    }
+}
